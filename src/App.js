9fa30e6283ca54abcf40ac6605f5cc854b3abb9a
Sandrine FEREY <sandrineferey@air-de-sandrine.lan>
2021-11-10 13:34:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Offer from "./pages/Offer";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [panier, setPanier] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://myfirst-vinted-api.herokuapp.com/"
+      );
+      // console.log(response.data);
+      setData(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement... </span>
+  ) : (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/offer">Offer</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Offer" element={<Offer />} />
+      </Routes>
+    </Router>
   );
 }
 
