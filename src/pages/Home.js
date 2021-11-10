66484b391009552e32a2794f317e2b5a920316f9
Sandrine FEREY <sandrineferey@air-de-sandrine.lan>
2021-11-10 18:19:45 +0100

    suite

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,30 @@
-const Home = ({ data }) => {
-  return (
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Home = () => {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://lereacteur-vinted-api.herokuapp.com/offers"
+        );
+
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement... </span>
+  ) : (
     <div className="container">
       <header>VINTED</header>
 
@@ -7,18 +32,21 @@ const Home = ({ data }) => {
         {data.offers.map((elem, index) => {
           return (
             <div key={index} className="products">
-              <div>
-                <img src={elem.product_image.url} alt="" />
-              </div>
-              <p>{elem.product_price}</p>
-              {elem.product_details.map((elem2, index) => {
-                return (
-                  <div key={index}>
-                    <p>{elem2.TAILLE}</p>
-                    <p>{elem2.MARQUE}</p>
-                  </div>
-                );
-              })}
+              <Link to={`/offer/${elem._id}`}>
+                Offer
+                <div>
+                  <img src={elem.product_image.url} alt="" />
+                </div>
+                <p>{elem.product_price}</p>
+                {elem.product_details.map((elem2, index) => {
+                  return (
+                    <div key={index}>
+                      <p>{elem2.TAILLE}</p>
+                      <p>{elem2.MARQUE}</p>
+                    </div>
+                  );
+                })}
+              </Link>
             </div>
           );
         })}
--- src/pages/Offer.js
+++ src/pages/Offer.js
@@ -1,12 +1,36 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Offer = () => {
-  const { offerId } = useParams();
+  //   const offerId = useParams();   {id: 5675765345636453698789}
+  const { id } = useParams(); //
 
-  return (
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://lereacteur-vinted-api.herokuapp.com/offer/${id}`
+        );
+
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement... </span>
+  ) : (
     <div>
-      <h2>Offer</h2>
-      <span>The product id is : {offerId}</span>
+      <img src={data.product_image.secure_url} alt="" />
+      <span>The product id is : {id}</span>
     </div>
   );
 };
